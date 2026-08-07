df1a4c512693b6231fc1483fd8e8cd423fe219b9
dayanacast <dayanacv80@gmail.com>
2026-08-07 12:56:16 -0600

    Agregar modulo de ordenes de trabajo

--- AutoFix/Entities/OrdenTrabajo.cs
+++ AutoFix/Entities/OrdenTrabajo.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AutoFix.Entities
 {
+    [Table("OrdenesTrabajo")]
     public class OrdenTrabajo
     {
         [Key]
---
+++ AutoFix/Migrations/202608071530074_SincronizarModeloOrdenTrabajo.cs
@@ -0,0 +1,16 @@
+﻿namespace AutoFix.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SincronizarModeloOrdenTrabajo : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
